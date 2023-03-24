c78dbec7b643896ecf6a0aa20d0056298113d621
Robson Carvalho <robson73904@gmail.com>
2023-03-24 15:31:19 -0300

    :sparkles: header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles";
+
+import { Header } from "./components/Header";
+
+import { cssVariables } from "./styles/cssVariables";
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={cssVariables}>
       <GlobalStyles />
-    </>
+      <Header />
+    </ThemeProvider>
   );
 };
