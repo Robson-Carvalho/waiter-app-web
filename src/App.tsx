c1064a2b7f77d8e8eed540053c51fce7c45f1157
Robson Carvalho <robson73904@gmail.com>
2023-03-24 16:45:48 -0300

    :sparkles: feat order component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
-import { Header } from "./components/Header";
-
 import { cssVariables } from "./styles/cssVariables";
+
+import { Header } from "./components/Header";
+import { Orders } from "./components/Orders";
 
 export const App = () => {
   return (
     <ThemeProvider theme={cssVariables}>
       <GlobalStyles />
       <Header />
+      <Orders />
     </ThemeProvider>
   );
 };
